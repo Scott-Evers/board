71ee3a3a35dcb2d8070bb9b64f4ef821ff14507c
Scott Evers <4837684-severs@users.noreply.gitlab.com>
2020-03-02 15:11:04 -0600

    gitignore updates

---
+++ terraform/iotcore.tf
@@ -0,0 +1,3 @@
+resource "aws_iot_thing_type" "board" {
+  name = "board"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_profile_backend" {
+    type = string
+}
+
+variable "aws_bucket_backend" {
+    type = string
+}
+
+variable "aws_profile" {
+    type = string
+}
+variable "aws_role_assumed" {
+    type = string
+}
